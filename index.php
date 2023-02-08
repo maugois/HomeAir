c53df3d4617a2ca62855651916a066d1ec5dcc48
mau_gois <gois.mauricio12@gmail.com>
2023-02-08 11:13:03 -0300

    top scroll

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
     <title></title>
 </head>
@@ -161,6 +162,7 @@
         </div>
     </section>
     <?php include 'rodape.html'?>
+    <script src="js/script.js"></script>
     <script>
         // Filtro produtos
         var site = angular.module('meuSite', []);
