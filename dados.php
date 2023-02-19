e55871bcd7b2e94dcfb5308b6a8188d500d64dca
mau_gois <gois.mauricio12@gmail.com>
2023-02-19 03:14:46 -0300

    dados para página detalhes

--- dados.php
+++ dados.php
@@ -2,41 +2,59 @@
 $dados = array(
     [
         "id" => 1,
-        "nome" => "",
-        "descricao" => "",
-        "preco" => 1000.99,
+        "nome" => "Ar-Condicionado 12000 BTUs Springer Midea Só Frio 220V High Wall Airvolution",
+        "descricao" => "Mais eficiência e economia de energia. Economiza energia, adequando-se à variações de temperatura do corpo durante o sono. Evita proliferação de odores e mofo garantindo um ar mais saudável.
+                        Com um sensor no controle remoto, a função permite que a temperatura desejada seja mantida onde está localizado o controle remoto.
+                        Ultra filter: Retém até 99% das bactérias. Filtro de nylon. Filtro de carvão ativado. Testado nas bactérias Escerichia coli e Staphylococcus aureus.
+                        Serpentina de cobre: Maior durabilidade e resistência contra os efeitos de chuva e maresia, estendendo a vida útil do produto.",
+        "preco" => 1.839,00,
         "image" => 'images/ar.png'
     ],
 
     [
         "id" => 2,
-        "nome" => "",
-        "descricao" => "",
-        "preco" => 1000.99,
+        "nome" => "Ar-Condicionado Hi Wall LG Dual Voice 12000 BTUs Quente/Frio",
+        "descricao" => "Mais eficiência e economia de energia. Economiza energia, adequando-se à variações de temperatura do corpo durante o sono. Evita proliferação de odores e mofo garantindo um ar mais saudável.
+                        Com um sensor no controle remoto, a função permite que a temperatura desejada seja mantida onde está localizado o controle remoto.
+                        Ultra filter: Retém até 99% das bactérias. Filtro de nylon. Filtro de carvão ativado. Testado nas bactérias Escerichia coli e Staphylococcus aureus.
+                        Serpentina de cobre: Maior durabilidade e resistência contra os efeitos de chuva e maresia, estendendo a vida útil do produto.",
+        "preco" => 1.582,24,
         "image" => 'images/ar2.png'
     ],
 
     [
         "id" => 3,
-        "nome" => "",
-        "descricao" => "",
-        "preco" => 1000.99,
+        "nome" => "Ar Condicionado Hi Wall LG Dual Compact 12.000 Btus Frio 220v",
+        "descricao" => "Ar Condicionado Hi Wall LG Dual Compact 12.000 Btus Frio 220v - S4UQ12JA3A5 Lg Dual Compact Tecnologia DUAL : O balanceamento do rotor duplo presente no 
+                        Compressor LG DUAL TM garante maior estabilidade e reduz a vibração, proporcionando uma economia de até 60% de energia e refrigeração até 30%mais rápida, através de um controle 
+                        mais amplo da velocidade. Até 60% de economia de energia: Com redução de velocidade, o DUAL reduz o número de partidas do compressor, economizando até 60% de energia, comparado com 
+                        um modelo convencional não. Refrigeração até 30% mais rápida: Graças ao controle mais amplo de velocidade do compressor, o LG DUAL atinge a temperatura desejada até 
+                        30% mais rápido e a mantém estável, sem a flutuação que ocorre em um modelo convencional, não. Super Silencioso: A partir de 19 dB: Com um sistema de ventilação de alta precisão, 
+                        o LG DUAL oferece um menor nível de ruído, a partir de 21 decibéis, sendo mais silencioso que uma biblioteca(36decibéis). Gold Fin Serpentina de Cobre Gold Fin: As aletas com acabamento GoldFinTM 
+                        proporcionam maior resistência a corrosão e estendem a vida útil do produto. Comfort Sleep: conforto térmico durante o sono : A função Com fort Sleep assegura o conforto térmico do ambiente durante o período de sono, 
+                        permitindo o ajuste de 4 diferentes funções automáticas em um clique.",
+        "preco" => 1.728,99,
         "image" => 'images/ar3.png'
     ],
 
     [
         "id" => 4,
-        "nome" => "",
-        "descricao" => "",
-        "preco" => 1000.99,
+        "nome" => "Ar Condicionado Hi Wall Fontaine 9000 BTU/h Frio - 127 Volts",
+        "descricao" => "Ar Condicionado Hi Wall Fontaine 9000 BTU/h Frio FOFT09F2R4CON01 - 127 Volts
+                        O ar condicionado Fontaine possui funções que proporcionam uma performance superior!
+                        Ideal para você que precisa de conforto térmico com praticidade e economia.
+                        Utiliza fluido refrigerante ecológico R-410A, que não agride a camada de ozônio, atóxico e inflamável.",
+        "preco" => 1.861,99,
         "image" => 'images/ar4.png'
     ],
 
     [
         "id" => 5,
-        "nome" => "",
-        "descricao" => "",
-        "preco" => 1000.99,
+        "nome" => "Ar Condicionado Hi Wall Springer Carrier Modernitá 9000 BTU/h Frio - 220 Volts",
+        "descricao" => "O Ar Condicionado Hi Wall Springer Carrier Modernitá 9000 BTU/h Frio 42AFVCG09D5 - 
+                        220 Volts traz conforto térmico com praticidade e economia. O sistema inverter reduz o consumo 
+                        de energia em até 60% em modo normal ou até 74% na função Eco Noite.",
+        "preco" => 2.335,50,
         "image" => 'images/ar5.png'
     ],
 
